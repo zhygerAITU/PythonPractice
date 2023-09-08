3e29802f3ba340726eed950a68e8e91660c00a26
zhygerAITU <zhyger.0001@gmail.com>
2023-09-08 15:33:24 +0600

    Update practice1.py

---
+++ practice1.py
@@ -0,0 +1,8 @@
+name = input("Your first name is: ")
+lastName = input("Your last name is: ")
+age = input("Your age is: ")
+location = input("Your location is: ")
+residence = input("Where do you live: ")
+
+print("Your last name - {}, and you first name - {}".format(lastName, name))
+print("Your age - {}, You live in - {}".format(age, location))
